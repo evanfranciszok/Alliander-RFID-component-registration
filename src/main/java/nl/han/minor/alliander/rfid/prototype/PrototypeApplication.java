3b2c369ba908254ea3a70e763fefc5341c34e1a4
Evan <git@evan.mailtje.net>
2021-11-10 16:24:20 +0100

    visual representation database as proof of concept

---
+++ src/main/java/nl/han/minor/alliander/rfid/prototype/PrototypeApplication.java
@@ -0,0 +1,12 @@
+package nl.han.minor.alliander.rfid.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeApplication.class, args);
+	}
+}
---
+++ src/main/java/nl/han/minor/alliander/rfid/prototype/persistence/TagDAO.java
@@ -0,0 +1,55 @@
+package nl.han.minor.alliander.rfid.prototype.persistence;
+
+import java.io.Serializable;
+
+public class TagDAO implements Serializable {
+
+  public TagDAO(long id, String supplier, String name, String dOI) {
+    this.id = id;
+    this.supplier = supplier;
+    this.name = name;
+    this.dateOfInstallment = dOI;
+  }
+
+  private long id;
+  private String supplier;
+  private String name;
+  private String dateOfInstallment;
+
+  @Override
+  public String toString() {
+    return id + supplier;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getSupplier() {
+    return supplier;
+  }
+
+  public void setSupplier(String supplier) {
+    this.supplier = supplier;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDateOfInstallment() {
+    return dateOfInstallment;
+  }
+
+  public void setDateOfInstallment(String dateOfInstallment) {
+    this.dateOfInstallment = dateOfInstallment;
+  }
+}
