94e6b5e57ffcee23b2b4cecab468cdbbfef106fd
Evan <git@evan.mailtje.net>
2021-11-17 12:27:27 +0100

    Added a test API call Proof of concept

--- src/main/java/nl/han/minor/alliander/rfid/prototype/persistence/PFScanner.java
+++ src/main/java/nl/han/minor/alliander/rfid/prototype/persistence/PFScanner.java
@@ -1,11 +1,23 @@
 package nl.han.minor.alliander.rfid.prototype.persistence;
 
+import java.net.URI;
+import java.net.http.*;
+import java.net.http.HttpResponse.BodyHandlers;
+
 import nl.han.minor.alliander.rfid.prototype.persistence.interfaces.IScanner;
+
+//https://openjdk.java.net/groups/net/httpclient/intro.html
 
 public class PFScanner implements IScanner {
 
   @Override
   public long[] scanTags() {
+
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://blockchain.info/ticker")).build();
+    client.sendAsync(request, BodyHandlers.ofString()).thenApply(HttpResponse::body).thenAccept(System.out::println)
+        .join();
+
     return new long[] { 54321, 12345, 13579 };
   }
 
