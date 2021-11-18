744961f8f0b77933b35f5c199b683133c8b04c0a
Evan <git@evan.mailtje.net>
2021-11-18 19:15:55 +0100

    Adding some other andpoint to the PFscanner

--- src/main/java/nl/han/minor/alliander/rfid/prototype/persistence/PFScanner.java
+++ src/main/java/nl/han/minor/alliander/rfid/prototype/persistence/PFScanner.java
@@ -15,6 +15,12 @@ import nl.han.minor.alliander.rfid.prototype.persistence.interfaces.IScanner;
 //https://mkyong.com/java/how-to-send-http-request-getpost-in-java/
 
 public class PFScanner implements IScanner {
+  private String bearerToken = null;
+  private String baseURL = "http://192.168.1.10/api/";
+  private String loginName = "admin";
+  private String loginPassword = "b0fc-4801-4a30-8d39-2c01-6b6a";
+  private final HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
+  private JSONParser parser = new JSONParser();
 
   /**
    * @return long[]
@@ -25,6 +31,13 @@ public class PFScanner implements IScanner {
     // getBearerToken();
     try {
       identification();
+      System.out.println("###########\nconfig");
+      readConfig();
+      System.out.println("###########\nData");
+      readData();
+      System.out.println("###########\nio");
+      getIOconfig();
+      System.out.println("###########\n\n");
     } catch (Exception e) {
       System.err.println(e);
     }
@@ -32,10 +45,104 @@ public class PFScanner implements IScanner {
     return new long[] { 54321, 12345, 13579 };
   }
 
-  private String bearerToken = null;
-  private String baseURL = "http://192.168.1.10/api/";
-  private final HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
-  private JSONParser parser = new JSONParser();
+  /**
+   * Log out from the system (I don't know if this does anything in the scanner.)
+   * 
+   * @throws Exception
+   */
+  private void logout() throws Exception {
+    HttpRequest request = createPostRequest("auth/logout",
+        "{ \"username\":\"admin\", \"password\":\"b0fc-4801-4a30-8d39-2c01-6b6a\" }");
+
+    HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+    System.out.println("Logging out attempt resulted with HTTP code: " + response.statusCode());
+  }
+
+  /**
+   * Returns the device identification
+   * 
+   * @throws Exception
+   */
+  private void identification() throws Exception {
+    HttpRequest request = createGetRequest("identification");
+    HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+    // print status code
+    System.out.println(parser.parse(response.body()));
+  }
+
+  /**
+   * Get input and output configurations
+   * 
+   * @throws Exception
+   */
+  private void getIOconfig() throws Exception {
+    HttpRequest request = createGetRequest("rfid/io");
+    System.out.println(request.uri());
+    HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+    System.out.println(response.statusCode());
+    // print status code
+    System.out.println(response.body());
+
+  }
+
+  /**
+   * Get the read transponder configuration
+   * 
+   * @throws Exception
+   */
+  private void readConfig() throws Exception {
+    HttpRequest request = createGetRequest("rfid/transponder/read");
+    System.out.println(request.uri());
+    HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+    System.out.println(response.statusCode());
+    // print status code
+    System.out.println(response.body());
+
+  }
+
+  /**
+   * Return transponder data from continous read
+   * 
+   * @throws Exception
+   */
+  private void readData() throws Exception {
+    HttpRequest request = createGetRequest("rfid/transponder/read/data");
+    HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+    System.out.println(response.statusCode());
+    // print status code
+    System.out.println(response.body());
+
+  }
+
+  /**
+   * Create a HTTPRequest object for a get request
+   * 
+   * @param url The relative URL (so without the base url) to the endpoint
+   * @return HttpRequest A Object for a get request to the correct endpoint
+   */
+  private HttpRequest createGetRequest(String url) {
+    return HttpRequest.newBuilder().GET().uri(URI.create(baseURL + url))
+        .setHeader("User-Agent", "Java 11 HttpClient Bot").header("Authorization", "Bearer " + getBearerToken())
+        .build();
+  }
+
+  /**
+   * Create a HttpRequest object for a post request
+   * 
+   * @param url  The relative URL (so without the base url) to the endpoint
+   * @param body
+   * @return HttpRequest
+   */
+  private HttpRequest createPostRequest(String url, String body) {
+    return HttpRequest.newBuilder().POST(BodyPublishers.ofString(body)).uri(URI.create(baseURL + url))
+        .setHeader("User-Agent", "Java 11 HttpClient Bot").header("Authorization", "Bearer " + getBearerToken())
+        .header("Content-Type", "application/json").build();
+  }
 
   /**
    * @return String
@@ -59,23 +166,14 @@ public class PFScanner implements IScanner {
   }
 
   /**
-   * @throws Exception
-   */
-  private void identification() throws Exception {
-    HttpRequest request = createGetRequest("identification");
-    HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
-    // print status code
-    System.out.println(parser.parse(response.body()));
-  }
-
-  /**
-   * @return String
+   * Log in to the system
+   * 
+   * @return String bearer token for the P+F scanner
    * @throws Exception
    */
   private String login() throws Exception {
     HttpRequest request = HttpRequest.newBuilder()
-        .POST(BodyPublishers.ofString("{ \"username\":\"admin\",\"password\":\"b0fc-4801-4a30-8d39-2c01-6b6a\" }"))
+        .POST(BodyPublishers.ofString("{ \"username\":\"" + loginName + "\",\"password\":\"" + loginPassword + "\" }"))
         .uri(URI.create(baseURL + "auth/login")).setHeader("User-Agent", "Java 11 HttpClient Bot")
         .header("Content-Type", "application/json").build();
 
@@ -86,41 +184,5 @@ public class PFScanner implements IScanner {
     if (json.containsKey("token"))
       return json.get("token").toString();
     return null;
-  }
-
-  /**
-   * Logging out of the P+F scanner. I don't know if this does anything in the
-   * scanner.
-   * 
-   * @throws Exception
-   */
-  private void logout() throws Exception {
-    HttpRequest request = createPostRequest("auth/logout",
-        "{ \"username\":\"admin\", \"password\":\"b0fc-4801-4a30-8d39-2c01-6b6a\" }");
-
-    HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
-    System.out.println("Logging out attempt resulted with HTTP code: " + response.statusCode());
-  }
-
-  /**
-   * @param url
-   * @return HttpRequest
-   */
-  private HttpRequest createGetRequest(String url) {
-    return HttpRequest.newBuilder().GET().uri(URI.create(baseURL + url))
-        .setHeader("User-Agent", "Java 11 HttpClient Bot").header("Authorization", "Bearer " + getBearerToken())
-        .build();
-  }
-
-  /**
-   * @param url
-   * @param body
-   * @return HttpRequest
-   */
-  private HttpRequest createPostRequest(String url, String body) {
-    return HttpRequest.newBuilder().POST(BodyPublishers.ofString(body)).uri(URI.create(baseURL + url))
-        .setHeader("User-Agent", "Java 11 HttpClient Bot").header("Authorization", "Bearer " + getBearerToken())
-        .header("Content-Type", "application/json").build();
   }
 }
